6794510084315d473cd7522f5b5b6a3f42976a09
sakai <pxj0684@gmail.com>
2024-12-16 16:31:36 +0900

    first

---
+++ sakai04/src/main/java/com/gitdemo/sakai04/Sakai04Application.java
@@ -0,0 +1,13 @@
+package com.gitdemo.sakai04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sakai04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sakai04Application.class, args);
+	}
+
+}
---
+++ sakai04/src/test/java/com/gitdemo/sakai04/Sakai04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitdemo.sakai04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sakai04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
